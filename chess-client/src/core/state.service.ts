1261d8c030afa407925fb5508216c14ae670fbbf
Jurre van der Laan <jurrevanderlaan@gmail.com>
2020-09-12 18:43:09 +0200

    simple chess board

--- chess-client/src/core/state.service.ts
+++ chess-client/src/core/state.service.ts
@@ -3,4 +3,31 @@ import { Injectable } from "@angular/core";
 @Injectable({
   providedIn: "root",
 })
-export class StateService {}
+export class StateService {
+  private readonly board = Array.from({ length: 8 }, (_, col) => Array.from({ length: 8 }, (_, row) => new Tile(7 - row, col)));
+
+  constructor() {
+    console.log(this.board);
+  }
+
+  get = () => this.board;
+}
+
+class Tile {
+  public selected: boolean = false;
+  public pieceid: string = null;
+  constructor(private row: number, private col: number) {}
+
+  text = () => `${this.row + 1}${this.columnToTekst.get(this.col)}`;
+
+  private columnToTekst = new Map([
+    [0, "a"],
+    [1, "b"],
+    [2, "c"],
+    [3, "d"],
+    [4, "e"],
+    [5, "f"],
+    [6, "g"],
+    [7, "h"],
+  ]);
+}
